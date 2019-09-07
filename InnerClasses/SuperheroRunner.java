55a3eb4a0756087551cc099d2f07becb09882ebe
Neel Shah <neelshah400@gmail.com>
2019-09-07 15:33:08 -0400

    Finish InnerClass Practice
    Part 3

--- InnerClasses/SuperheroRunner.java
+++ InnerClasses/SuperheroRunner.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class SuperheroRunner
 {
 
@@ -9,16 +11,38 @@ public class SuperheroRunner
 		String name = "Iron Man";
 		System.out.println(superhero.isEnemy(name) ? name + " is an enemy." : name + " is not an enemy.");
 		superhero.attacked();
-		System.out.println(superhero.getName() + " has " + superhero.getHealth() + " health remaining.");
+		System.out.println(superhero.getName() + " has " + superhero.getHealth() + " health remaining.\n");
 
 		Thor thor = new Thor(100);
 		System.out.println(thor);
 		Thor.Hammer hammer = new Thor.Hammer();
-		System.out.println(hammer);
+		System.out.println(hammer + "\n");
+
+		ArrayList<Superhero> list = new ArrayList<Superhero>();
+		for(int i = 0; i < 5; i++)
+			list.add(new Superhero());
+		for(Superhero hero : list)
+		{
+			hero.attacked();
+		}
+		System.out.println(list);
+		for(int i = 0; i < list.size() - 1; i++)
+		{
+			int minIndex = i;
+			for(int j = i; j < list.size(); j++)
+			{
+				if(list.get(j).compareTo(list.get(minIndex)) < 0)
+					minIndex = j;
+			}
+			Superhero temp = list.get(i);
+			list.set(i, list.get(minIndex));
+			list.set(minIndex, temp);
+		}
+		System.out.println(list);
 
 	}
 
-	public static class Superhero
+	public static class Superhero implements Comparable<Superhero>
 	{
 
 		private String name, power, enemy;
@@ -56,7 +80,17 @@ public class SuperheroRunner
 
 		public String toString()
 		{
-			return name + " will fights his enemy " + enemy + " with his " + power + ".";
+			return name + " has " + health + " health.";
+		}
+
+		public int compareTo(Superhero obj)
+		{
+			if(health > obj.getHealth())
+				return 1;
+			else if(health < obj.getHealth())
+				return -1;
+			else
+				return getName().compareTo(obj.getName());
 		}
 
 	}
