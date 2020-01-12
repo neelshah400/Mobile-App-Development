93f9550b6b656b0a5b7a30b6379562b377492de2
Neel Shah <neelshah400@gmail.com>
2020-01-12 11:24:17 -0500

    Weather: Image and Description

--- Weather/app/src/main/java/com/example/weather/MainActivity.java
+++ Weather/app/src/main/java/com/example/weather/MainActivity.java
@@ -2,11 +2,11 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
@@ -22,10 +22,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textZipCode, textCity, textTemperature;
+    TextView textZipCode, textCity, textDateTime, textTemperature, textDescription;
     EditText editZipCode;
     ImageButton buttonSearch;
     ImageView imageWeather;
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     String zipCode;
     JSONObject jsonWeather, jsonForecast;
     Drawable drawable;
+    int image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +46,9 @@ public class MainActivity extends AppCompatActivity {
 
         textZipCode = findViewById(R.id.id_textZipCode);
         textCity = findViewById(R.id.id_textCity);
+        textDateTime = findViewById(R.id.id_textDateTime);
         textTemperature = findViewById(R.id.id_textTemperature);
+        textDescription = findViewById(R.id.id_textDescription);
         editZipCode = findViewById(R.id.id_editZipCode);
         buttonSearch = findViewById(R.id.id_buttonSearch);
         imageWeather = findViewById(R.id.id_imageWeather);
@@ -101,11 +107,65 @@ public class MainActivity extends AppCompatActivity {
 
                 String icon = jsonWeather.getJSONArray("weather").getJSONObject(0).getString("icon");
                 Log.d("SHAH", icon);
-//                URL iconUrl = new URL("http://openweathermap.org/img/wn/" + icon + "@2x.png");
-                URL iconUrl = new URL("https://cdn1.iconfinder.com/data/icons/weather-429/64/weather_icons_color-06-512.png");
                 Log.d("SHAH", "http://openweathermap.org/img/wn/" + icon + "@2x.png");
-                InputStream inputStream = (InputStream) iconUrl.getContent();
-                drawable = Drawable.createFromStream(inputStream, "src");
+
+                switch(icon) {
+                    case "01d":
+                        image = R.drawable.sunny;
+                        break;
+                    case "01n":
+                        image = R.drawable.clear;
+                        break;
+                    case "02d":
+                        image = R.drawable.mostly_sunny;
+                        break;
+                    case "02n":
+                        image = R.drawable.mostly_clear;
+                        break;
+                    case "03d":
+                        image = R.drawable.partly_sunny;
+                        break;
+                    case "03n":
+                        image = R.drawable.partly_clear;
+                        break;
+                    case "04d":
+                        image = R.drawable.mostly_cloudy_day;
+                        break;
+                    case "04n":
+                        image = R.drawable.mostly_cloudy_night;
+                        break;
+                    case "09d":
+                        image = R.drawable.showers_day;
+                        break;
+                    case "09n":
+                        image = R.drawable.showers_night;
+                        break;
+                    case "10d":
+                        image = R.drawable.rain_day;
+                        break;
+                    case "10n":
+                        image = R.drawable.rain_night;
+                        break;
+                    case "11d":
+                        image = R.drawable.thunderstorm_day;
+                        break;
+                    case "11n":
+                        image = R.drawable.thunderstorm_night;
+                        break;
+                    case "13d":
+                        image = R.drawable.snow_day;
+                        break;
+                    case "13n":
+                        image = R.drawable.snow_night;
+                    case "50d":
+                        image = R.drawable.fog_day;
+                        break;
+                    case "50n":
+                        image = R.drawable.fog_night;
+                    default:
+                        Log.d("SHAH", "error");
+                        break;
+                }
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -121,8 +181,25 @@ public class MainActivity extends AppCompatActivity {
             try {
 
                 textCity.setText(jsonWeather.getString("name"));
-                textTemperature.setText(Math.round(Double.parseDouble(jsonWeather.getJSONObject("main").getString("temp"))) + "°");
-                imageWeather.setImageDrawable(drawable);
+                textTemperature.setText(Math.round(jsonWeather.getJSONObject("main").getDouble("temp")) + "°");
+                imageWeather.setImageResource(image);
+
+                String description = jsonWeather.getJSONArray("weather").getJSONObject(0).getString("description");
+                String [] words = description.split(" ");
+                description = "";
+                for (String word : words) {
+                    word = word.substring(0, 1).toUpperCase() + word.substring(1);
+                    description += word + " ";
+                }
+                textDescription.setText(description);
+
+                long unixTime = jsonWeather.getLong("dt");
+                Date date = new Date(unixTime * 1000);
+                SimpleDateFormat sdf = new SimpleDateFormat("EEE, MMM DD, yyyy - HH:mm a");
+                sdf.setTimeZone(TimeZone.getTimeZone("GMT-5"));
+                String formattedDate = sdf.format(date);
+                textDateTime.setText(formattedDate);
+                Log.d("SHAH", formattedDate);
 
             } catch (Exception e) {
                 e.printStackTrace();
