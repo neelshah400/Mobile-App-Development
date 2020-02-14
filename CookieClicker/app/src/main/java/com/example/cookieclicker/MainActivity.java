bf91fbb48b7897784adc95f87a521bdb18a560f1
Neel Shah <10014194@sbschools.org>
2020-02-14 12:13:26 -0500

    Changes

--- CookieClicker/app/src/main/java/com/example/cookieclicker/MainActivity.java
+++ CookieClicker/app/src/main/java/com/example/cookieclicker/MainActivity.java
@@ -5,10 +5,15 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
@@ -16,17 +21,20 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.Timer;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout constraintLayout;
             
-    TextView textMoney, textIncome;
+    static TextView textMoney, textIncome;
     ImageView imageCorn;
     
     TextView textPlus;
 
-    int money = 0;
-    int income = 0;
+    static AtomicInteger money = new AtomicInteger();
+    static AtomicInteger income = new AtomicInteger();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,17 +47,18 @@ public class MainActivity extends AppCompatActivity {
         textMoney = findViewById(R.id.id_textMoney);
         textIncome = findViewById(R.id.id_textIncome);
         imageCorn = findViewById(R.id.id_imageCorn);
-        
-        final ScaleAnimation scaleAnimation = new ScaleAnimation(1.5f, 1.0f, 1.5f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        scaleAnimation.setDuration(400);
+
+        income.set(1);
 
         imageCorn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                ScaleAnimation scaleAnimation = new ScaleAnimation(1.5f, 1.0f, 1.5f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                scaleAnimation.setDuration(400);
+                imageCorn.startAnimation(scaleAnimation);
                 
-                v.startAnimation(scaleAnimation);
-                
-                money++;
+                money.getAndAdd(1);
                 textMoney.setText(money + " cobs");
 
                 textPlus = new TextView(MainActivity.this);
@@ -65,9 +74,6 @@ public class MainActivity extends AppCompatActivity {
                 ConstraintSet constraintSet = new ConstraintSet();
                 constraintSet.clone(constraintLayout);
 
-                constraintSet = new ConstraintSet();
-                constraintSet.clone(constraintLayout);
-
                 constraintSet.connect(textPlus.getId(), ConstraintSet.TOP, imageCorn.getId(), ConstraintSet.TOP);
                 constraintSet.connect(textPlus.getId(), ConstraintSet.BOTTOM, imageCorn.getId(), ConstraintSet.BOTTOM);
                 constraintSet.connect(textPlus.getId(), ConstraintSet.LEFT, imageCorn.getId(), ConstraintSet.LEFT);
@@ -78,14 +84,46 @@ public class MainActivity extends AppCompatActivity {
 
                 constraintSet.applyTo(constraintLayout);
 
+                AnimationSet animationSet = new AnimationSet(false);
+
                 TranslateAnimation translateAnimation = new TranslateAnimation(0, 0, 0, -600);
                 translateAnimation.setDuration(1000);
-                textPlus.startAnimation(translateAnimation);
+                animationSet.addAnimation(translateAnimation);
+
+                AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0.0f);
+                alphaAnimation.setInterpolator(new AccelerateInterpolator());
+                alphaAnimation.setDuration(1000);
+                animationSet.addAnimation(alphaAnimation);
+
+                textPlus.startAnimation(animationSet);
                 textPlus.setVisibility(View.INVISIBLE);
                 
             }
         });
 
     }
+
+    public static class BackgroundThread extends HandlerThread {
+
+        Handler handler;
+
+        public BackgroundThread(String name) {
+            super(name);
+        }
+
+        public void run() {
+
+            try {
+                money.getAndAdd(income.get());
+                textMoney.setText(money + " cobs");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+
 
 }
